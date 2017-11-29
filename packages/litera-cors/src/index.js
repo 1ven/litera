fe53b603d8f6f7b54d969dd1aa5e8a63bde9a17e
1ven <1ven.mironchik@gmail.com>
2017-11-29 17:10:48 +0300

    Add cors package

---
+++ packages/litera-cors/src/index.js
@@ -0,0 +1,20 @@
+export default atom => async req => {
+  const res = await atom(req);
+  return req.method === "OPTIONS"
+    ? {
+        status: 204,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-type,Accept",
+          "Access-Control-Allow-Methods": "GET,PUT,PATCH,POST,DELETE,OPTIONS",
+          "Content-Length": "0"
+        }
+      }
+    : {
+        ...res,
+        headers: {
+          ...res.headers,
+          "Access-Control-Allow-Origin": "*"
+        }
+      };
+};
