caea1f456dea1af92ae3f204f62763249c4b02bf
1ven <1ven.mironchik@gmail.com>
2017-11-30 23:59:05 +0300

    Add litera error handler

---
+++ packages/litera-error-handler/src/index.js
@@ -0,0 +1,13 @@
+export default () => atom => async req => {
+  try {
+    return await atom(req);
+  } catch (err) {
+    return {
+      status: 500,
+      body:
+        process.env.NODE_ENV === production
+          ? ""
+          : err.stack || err.message || JSON.stringify(err)
+    };
+  }
+};
