db5b19c46ffc01958a17f3f087d0e6055eeab6da
1ven <1ven.mironchik@gmail.com>
2017-11-30 23:10:15 +0300

    Not parsing requests without content type

--- packages/litera-body-parser/src/index.js
+++ packages/litera-body-parser/src/index.js
@@ -2,7 +2,13 @@ import contentType from "content-type";
 import { json } from "./types";
 
 export default () => atom => req => {
-  const { type } = contentType.parse(req.headers["content-type"]);
+  const contentType = req.headers["content-type"];
+
+  if (!contentType) {
+    return atom(req);
+  }
+
+  const { type } = contentType.parse(contentType);
 
   switch (type) {
     case "application/json":
