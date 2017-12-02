0f8f11c3b6aa10a27ad2cb5dbf128308128acdf4
1ven <1ven.mironchik@gmail.com>
2017-12-02 15:55:14 +0300

    Add response funcs. Export merge data

--- packages/litera/src/index.js
+++ packages/litera/src/index.js
@@ -1,1 +1,2 @@
 export { default as adapter } from "./adapter";
+export { default as merge, withStatus, withHeader, withBody } from "./merge";
--- packages/litera/src/merge.js
+++ packages/litera/src/merge.js
@@ -2,3 +2,7 @@ import merge from "deepmerge";
 import isPlainObject from "is-plain-object";
 
 export default (...items) => merge.all(items.filter(isPlainObject));
+
+export const withStatus = status => ({ status });
+export const withBody = body => ({ body });
+export const withHeader = (k, v) => ({ headers: { [k]: v } });
